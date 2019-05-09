2abadb94b4c2f80238400777b09fe9bc94aef5d5
Sergio <sercar88@gmail.com>
2019-05-09 12:26:13 -0500

    test

---
+++ src/ChocolateBoiler.java
@@ -0,0 +1,49 @@
+public class ChocolateBoiler {
+
+    private static ChocolateBoiler instance;
+
+    private boolean empty;
+    private boolean boiled;
+
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public static ChocolateBoiler getInstance(){
+        if (instance == null){
+            instance = new ChocolateBoiler();
+        }
+        return instance;
+
+    }
+    public void fill(){
+
+        if(isEmpty()){
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain(){
+        if(isEmpty() && isBoiled()){
+            //drain the boiled milk and chocolate
+            empty = true;
+        }
+    }
+
+    public void boil(){
+        if(!isEmpty() && isBoiled()){
+            //bring the contents to a boil
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty(){
+        return empty;
+    }
+
+    public boolean isBoiled(){
+        return boiled;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        ChocolateBoiler balde= ChocolateBoiler.getInstance();
+
+        FalsechocolateBoiler balde2 = new FalsechocolateBoiler();
+
+        System.out.println("Hello World!");
+
+    }
+
+
+}
